d7a9d8ca4feeb1d32102edd3d86e485824106b62
Gustav <sir.gustav.the.coder@gmail.com>
2014-11-18 23:51:49 +0100

    updated test code

--- infofile/test.cpp
+++ infofile/test.cpp
@@ -3,6 +3,7 @@
 #include "infofile/infofile.h"
 
 void main() {
+	std::vector<std::string> errors;
 	infofile::Value val;
-	infofile::Parse("data { key=value; }", &val);
+	infofile::Parse("inline", "@data { key=value; }", &val, &errors);
 }
