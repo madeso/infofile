df6f621e6b16f852c1fd6363b3d6706ba2d4e9b7
Gustav <sir.gustav.the.coder@gmail.com>
2014-11-20 08:31:49 +0100

    more testing

--- infofile/test.cpp
+++ infofile/test.cpp
@@ -61,4 +61,70 @@ namespace test
 		EXPECT_EQ(0, Node::ActiveCount());
 		EXPECT_EQ(0, Value::ActiveCount());
 	}
+
+	GTEST(testparsing_twokeys)
+	{
+		std::vector<std::string> errors;
+		infofile::Value* val = infofile::Parse("inline", "{key value k v}", &errors);
+
+		EXPECT_EQ(0, errors.size());
+		//ASSERT_NE(NULL, val);
+		ASSERT_EQ(2, val->children.size());
+
+		EXPECT_EQ("key", val->children[0]->name());
+		EXPECT_EQ("value", val->children[0]->value());
+
+		EXPECT_EQ("k", val->children[1]->name());
+		EXPECT_EQ("v", val->children[1]->value());
+
+		delete val;
+
+		EXPECT_EQ(0, Node::ActiveCount());
+		EXPECT_EQ(0, Value::ActiveCount());
+	}
+
+	GTEST(testparsing_array)
+	{
+		std::vector<std::string> errors;
+		infofile::Value* val = infofile::Parse("inline", "[value v]", &errors);
+
+		EXPECT_EQ(0, errors.size());
+		//ASSERT_NE(NULL, val);
+		ASSERT_EQ(2, val->children.size());
+
+		EXPECT_EQ("", val->children[0]->name());
+		EXPECT_EQ("value", val->children[0]->value());
+
+		EXPECT_EQ("", val->children[0]->name());
+		EXPECT_EQ("v", val->children[0]->value());
+
+		delete val;
+
+		EXPECT_EQ(0, Node::ActiveCount());
+		EXPECT_EQ(0, Value::ActiveCount());
+	}
+
+	GTEST(testparsing_subkey)
+	{
+		std::vector<std::string> errors;
+		infofile::Value* val = infofile::Parse("inline", "[{key value}]", &errors);
+
+		EXPECT_EQ(0, errors.size());
+		//ASSERT_NE(NULL, val);
+		ASSERT_EQ(1, val->children.size());
+
+		Node* n = val->children[0];
+		EXPECT_EQ("", n->name());
+		EXPECT_EQ("", n->value());
+		Value* v = n->children;
+		ASSERT_EQ(1, v->children.size());
+
+		EXPECT_EQ("key", v->children[0]->name());
+		EXPECT_EQ("value", v->children[0]->value());
+
+		delete val;
+
+		EXPECT_EQ(0, Node::ActiveCount());
+		EXPECT_EQ(0, Value::ActiveCount());
+	}
 }
