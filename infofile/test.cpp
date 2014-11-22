d417a9bd6bad198930b85bf392208bb0e4cf1d2c
Gustav <sir.gustav.the.coder@gmail.com>
2014-11-22 11:15:33 +0100

    testing ' in " strings

--- infofile/test.cpp
+++ infofile/test.cpp
@@ -134,13 +134,13 @@ namespace test
 	GTEST(test_basic_string)
 	{
 		std::vector<std::string> errors;
-		infofile::Value* val = infofile::Parse("inline", "{\"key\"=\"value\";}", &errors);
+		infofile::Value* val = infofile::Parse("inline", "{\"'key'\"=\"value\";}", &errors);
 
 		EXPECT_EQ(0, errors.size());
 		EXPECT_TRUE(val != NULL);
 		ASSERT_EQ(1, val->children.size());
 
-		EXPECT_EQ("key", val->children[0]->name());
+		EXPECT_EQ("'key'", val->children[0]->name());
 		EXPECT_EQ("value", val->children[0]->value());
 
 		delete val;
