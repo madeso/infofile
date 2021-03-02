107925ac35c898b32f9291f1e562da9eb6eab85a
Gustav <sir.gustav.the.coder@gmail.com>
2021-03-02 21:58:48 +0100

    improved printstring so that it works for all simple idents

--- src/infofile/printstring.cc
+++ src/infofile/printstring.cc
@@ -2,21 +2,100 @@
 
 #include <sstream>
 
+#include "fmt/core.h"
+
 namespace infofile
 {
+    namespace
+    {
+        bool IsNumber(char c)
+        {
+            return '0' <= c && c <= '9';
+        }
+
+        bool IsAlpha(char c)
+        {
+            const auto lower = 'a' <= c && c <= 'z';
+            const auto upper = 'A' <= c && c <= 'Z';
+            return upper || lower;
+        }
+
+        bool IsIdentChar(char c, bool first)
+        {
+            if (first)
+            {
+                const auto lower = 'a' <= c && c <= 'z';
+                const auto upper = 'A' <= c && c <= 'Z';
+                return lower || upper || c == '_';
+            }
+            else
+            {
+                const auto number = IsNumber(c);
+                const auto special = c == '.' || c == '@';
+                return IsIdentChar(c, true) || number || special;
+            }
+        }
+
+        bool IsIdent(const std::string& str)
+        {
+            if (str.empty())
+            {
+                return false;
+            }
+
+            bool first = true;
+
+            for (char c : str)
+            {
+                if (IsIdentChar(c, first) == false)
+                {
+                    return false;
+                }
+                first = false;
+            }
+
+            return true;
+        }
+
+        std::string CharToCharInString(char c)
+        {
+            switch (c)
+            {
+            case '\0':
+                return "\\0";
+            case '\n':
+                return "\\n";
+            case '\r':
+                return "\\r";
+            case '"':
+                return "\\\"";
+            case '\t':
+                return "\\t";
+            case '\\':
+                return "\\\\";
+            default:
+                return fmt::format("{}", c);
+            }
+        }
+    }
+
     std::string PrintString(const std::string& str)
     {
-        std::ostringstream ss;
-        std::string::size_type index = str.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-        if (index == -1 && str.empty() == false)
+        if (IsIdent(str))
         {
-            ss << str;
+            return str;
         }
         else
         {
-            ss << "\"" << str << "\"";
+            std::ostringstream ss;
+            ss << "\"";
+            for (auto i = 0; i < str.size(); i += 1)
+            {
+                const char c = str[i];
+                ss << CharToCharInString(c);
+            }
+            ss << "\"";
+            return ss.str();
         }
-        return ss.str();
     }
-
 }
--- src/infofile/printstring.test.cc
+++ src/infofile/printstring.test.cc
@@ -20,4 +20,37 @@ TEST_CASE("print string tests", "[printstring]")
     {
         REQUIRE(catchy::StringEq(PrintString("a b c"), "\"a b c\""));
     }
+
+    SECTION("escapes")
+    {
+        SECTION("quote")
+        {
+            REQUIRE(catchy::StringEq(PrintString("\""), "\"\\\"\""));
+        }
+        SECTION("newline")
+        {
+            REQUIRE(catchy::StringEq(PrintString("\n"), "\"\\n\""));
+        }
+        SECTION("linefeed")
+        {
+            REQUIRE(catchy::StringEq(PrintString("\r"), "\"\\r\""));
+        }
+        SECTION("tab")
+        {
+            REQUIRE(catchy::StringEq(PrintString("\t"), "\"\\t\""));
+        }
+        SECTION("null")
+        {
+            REQUIRE(catchy::StringEq(PrintString(std::string(1, '\0')), "\"\\0\""));
+        }
+        SECTION("backslash")
+        {
+            REQUIRE(catchy::StringEq(PrintString("\\"), "\"\\\\\""));
+        }
+    }
+
+    SECTION("email")
+    {
+        REQUIRE(catchy::StringEq(PrintString("hello@hotmale.com"), "hello@hotmale.com"));
+    }
 }
